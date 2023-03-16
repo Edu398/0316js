ad785a4de5508a9cb20f1477929e9461849c9994
Edu398 <fejes.edu@gmail.com>
2023-03-16 15:16:05 +0100

    0316js

---
+++ success.js
@@ -0,0 +1,4 @@
+const ARTICLEELEM = document.querySelectorAll("article");
+for (let index = 0; index < kepLista.length; index++) {
+    ARTICLEELEM[0].innerHTML += <div ><img src=kepLista[index];
+}
